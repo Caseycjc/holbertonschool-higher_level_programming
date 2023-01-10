f2247f68f2c69270e9bf573170d0f976787f4fb6
root <knightlaggia@gmail.com>
2023-01-10 12:01:17 -0800

    changed things

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-l = 0
-u = 99
-
-for num in range(l, u + 1):
-    print(num)
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
