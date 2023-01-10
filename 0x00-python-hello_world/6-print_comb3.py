4316318ee779ddcd07cf0bb36224264c3b802376
root <knightlaggia@gmail.com>
2023-01-10 12:39:24 -0800

    task 8

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -6,4 +6,4 @@ for i in range(0, 10):
         elif i == 8 and j == 9:
             print("{}{}".format(i, j))
         else:
-            print("{}{}, ".format(i, j) end="")
+            print("{}{}, ".format(i, j), end="")
