09828e3556be8ea516fd41f8956453edcd5c7d64
root <knightlaggia@gmail.com>
2023-01-25 15:16:23 -0800

    square

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -4,11 +4,11 @@ This module contains a class that defines a square.
 """
 
 
-class Square():
-    """Square"""
+class Square:
+    """Creates A Square"""
     def __init__(self, size=0):
-        """intializes square"""
-        if type(self.__size) is not int:
+        """Initializes Square"""
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError("size must be >= 0")
