2b0822fcbf4959aeb9c7c6081187f17ca1dd0315
root <knightlaggia@gmail.com>
2023-01-25 18:26:57 -0800

    rekt

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -6,5 +6,30 @@ This module contains a class that defines a rectangle.
 
 class Rectangle:
     """Defines a rectangle."""
-    def __init__(self):
-        pass
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("width must be an integer")
+        if (0 > value):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        if (0 > value):
+            raise ValueError("height must be >= 0")
+        self.__height = value
