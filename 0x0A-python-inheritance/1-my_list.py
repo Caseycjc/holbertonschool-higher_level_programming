204aac8b7fca52df98084c45cfb6dbc749ba7ecc
root <knightlaggia@gmail.com>
2023-01-26 12:05:27 -0800

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module that inherits a sorted list"""
+
+
+
+class MyList(list):
+    """class that is inheriting a list"""
+    pass
+
+def print_sorted(self):
+    """prints a sorted list"""
+
+    print(sorted(list(self)))
