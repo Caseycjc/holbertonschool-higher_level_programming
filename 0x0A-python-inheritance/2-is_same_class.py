e0fcec5fe4a5a071b917a9d679ad4c660e6dca1d
root <knightlaggia@gmail.com>
2023-01-27 08:45:01 -0800

    inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,8 +3,9 @@
 is exactly an instance of a class"""
 
 
+
 def is_same_class(obj, a_class):
     """ function that returns true of false
 depending on the object"""
 
-return type(obj) is a_class
+    return type(obj) is a_class
