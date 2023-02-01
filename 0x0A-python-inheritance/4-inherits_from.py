8de32e983f42922821a9401917719423d40fc035
root <knightlaggia@gmail.com>
2023-01-31 20:41:35 -0800

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module with method inherits_from
+"""
+
+
+
+def inherits_from(obj, a_class):
+    """Returns true if the object is inherited from a specified class"""
+
+    return False if type(obj) is a_class else isinstance(obj, a_class)
