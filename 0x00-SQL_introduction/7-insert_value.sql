65857732e2d683ce25397fdb211c2b3b84f061f3
root <knightlaggia@gmail.com>
2023-02-13 20:14:08 -0800

    sql

--- 0x00-SQL_introduction/7-insert_value.sql
+++ 0x00-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- script that inserts a new row in a table
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Holberton School");
