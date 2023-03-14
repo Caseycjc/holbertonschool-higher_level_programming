48e61817755be295afb1ad33c8631eb36e19eac8
CaseyCJC <knightlaggia@gmail.com>
2023-03-14 01:17:53 +0000

    trying this

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Pthon script that fetches a webpage"""
+"""Python script that fetches a webpage"""
 import urllib.request
 
 
-def my_Status():
-    """class to fetch the webpage"""
+if __name__ == "__main__":
     req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         html = response.read()
@@ -12,6 +11,3 @@ def my_Status():
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
-
-if __name__ == "__main__":
-    my_Status()
