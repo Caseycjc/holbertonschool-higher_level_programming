1938280970b503ddf1b7578d9013e8f44f89d443
CaseyCJC <knightlaggia@gmail.com>
2023-03-14 01:32:50 +0000

    trying this

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Python script that fetches a webpage"""
+"""
+Python script that fetches a webpage
+"""
 import urllib.request
 
 
@@ -8,6 +10,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         html = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
