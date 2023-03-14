59cb6766a08ba38c05621114bd3dc1450e7d630b
CaseyCJC <knightlaggia@gmail.com>
2023-03-14 01:36:00 +0000

    trying this

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Python script that fetches a webpage
 """
 import urllib.request
 
+url = 'https://intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print("Body response:")
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode('utf-8')))
