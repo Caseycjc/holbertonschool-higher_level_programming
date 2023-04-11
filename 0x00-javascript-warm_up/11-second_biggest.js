abdbfb163c9c4b589b5d1a8b6632feeb5f2e2311
CaseyCJC <knightlaggia@gmail.com>
2023-04-11 08:37:43 +0000

    task11

---
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv;
+if (args.length <= 3) {
+  console.log('0');
+} else {
+  args.splice(0, 2);
+  const second = secondBiggest(args);
+  console.log(second);
+}
+
+function secondBiggest (list) {
+  const max = list.sort((a, b) => a - b);
+  return max[max.length - 2];
+}
