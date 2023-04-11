af456d70bf98c37b10a7552e8a6d25a5c8364c6c
CaseyCJC <knightlaggia@gmail.com>
2023-04-11 04:54:57 +0000

    3

---
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVarC = 'C is fun';
+const myVarP = 'Python is cool';
+const myVarJ = 'JavaScript is amazing';
+console.log(myVarC);
+console.log(myVarP);
+console.log(myVarJ);
