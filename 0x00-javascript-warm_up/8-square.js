ee9c3a83fef4b3bc5dbc8cb98d674c90b1718546
CaseyCJC <knightlaggia@gmail.com>
2023-04-11 08:26:16 +0000

    task8

---
+++ 0x00-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined) {
+    console.log('Missing size');
+  } else if (isNaN(process.argv[2])) {
+    console.log('Missing size');
+  } else {
+    for (let i = 1; i <= process.argv[2]; i++) {
+      console.log('X'.repeat(process.argv[2]));
+    }
+  }
