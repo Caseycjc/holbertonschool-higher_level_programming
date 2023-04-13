da682d0e21a1fb070471e92a3ba0fb6044a4f6c7
CaseyCJC <knightlaggia@gmail.com>
2023-04-13 18:09:05 +0000

    task1

---
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,20 @@
+#!/bin/usr/node
+const fs = require('fs');
+
+function writeStringToFile (filePath, stringToWrite) {
+  fs.writeFile(filePath, stringToWrite, 'utf8', (error) => {
+    if (error) {
+      console.error('An error occurred:', error);
+    } else {
+      console.log(`Successfully wrote to file: ${filePath}`);
+    }
+  });
+}
+
+if (process.argv.length > 3) {
+  const filePath = process.argv[2];
+  const stringToWrite = process.argv[3];
+  writeStringToFile(filePath, stringToWrite);
+} else {
+  console.error('Please provide a file path and a string to write as arguments');
+}
