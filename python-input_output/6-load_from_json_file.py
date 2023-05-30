a8d1fe282baec0e4c74ccd250788ad780e7a4303
Caseycjc <knightlaggia@gmail.com>
2023-05-30 17:37:52 -0500

    task6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,7 +7,7 @@ function that creates an object from a JSON file
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """write an object to a text file"""
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+def load_from_json_file(filename):
+    """creates the object"""
+    with open(filename, 'r') as file:
+        return json.load(file)
