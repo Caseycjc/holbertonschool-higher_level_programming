d717754fcc285f2411f9271995ff5ad695bb0689
Caseycjc <knightlaggia@gmail.com>
2023-05-30 17:58:36 -0500

    task8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that returns the dictionary desc
+with simple data structure for JSON serialization of an object"""
+
+
+def class_to_json(obj):
+    """converts the object"""
+    if not hasattr(obj, "__dict__"):
+        return {}
+    
+    json_dict = {}
+    for key, value in obj.__dict__.items():
+        if not key.startswith("__"):
+            json_dict[key] = value
+
+    return json_dict
