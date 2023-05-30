28e3ef24d8518baf4ca2aee143cdd118b19b7a6a
Caseycjc <knightlaggia@gmail.com>
2023-05-30 18:06:59 -0500

    task9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class that defines a student"""
+
+
+class Student:
+    """class that reps a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self):
+            """gets a dictionary rep of a student instance"""
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
