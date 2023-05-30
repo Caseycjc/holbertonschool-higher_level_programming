5f9e691bebda7da4c509e9a3be9c89dd4849514d
Caseycjc <knightlaggia@gmail.com>
2023-05-30 16:51:16 -0500

    Repo, README, and first task

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Function that reads a text file
+"""
+
+def read_file(filename=""):
+    """reads the file and prints it"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end='')
