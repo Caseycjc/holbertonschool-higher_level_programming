c168669dd446a27143d93a7ae0b3e182556e9600
Caseycjc <knightlaggia@gmail.com>
2023-05-30 23:24:18 -0500

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+geometry class
+"""
+
+
+class BaseGeometry:
+    """base class"""
+
+    def area(self):
+        """computes the area"""
+    raise Exception("area() is not implemented") 
