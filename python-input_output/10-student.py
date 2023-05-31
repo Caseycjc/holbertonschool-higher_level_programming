92d73da9d867ccf67aea08f3f650dd9ac28b0e69
Caseycjc <knightlaggia@gmail.com>
2023-05-30 22:42:40 -0500

    task10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Class that defines a student
+"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of the Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
