c50592531968c496089dd12cb2688152eb919d3e
Caseycjc <knightlaggia@gmail.com>
2023-05-30 22:46:58 -0500

    task11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Defines a student
+"""
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of the Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
