47ad1b7684e4db51c3644410b100aaafb8c9ef02
Caseycjc <knightlaggia@gmail.com>
2023-05-30 22:47:31 -0500

    task11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -17,7 +17,8 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
 
     def reload_from_json(self, json):
         """Replaces all attributes of the Student instance"""
