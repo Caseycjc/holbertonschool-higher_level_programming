2b28a96b47368a71ae4d754bc414da3305e4a244
Caseycjc <knightlaggia@gmail.com>
2023-05-31 10:19:42 -0500

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,17 +5,17 @@ script that adds all args to a list and then save them
 
 
 import sys
-import json
-from typing import List
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    my_list = load_from_json_file("add_item.json")
-except(TypeError, FileNotFoundError):
-    my_list = []
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-save_to_json_file(my_list, "add_item.json")
+
+def add_item(items={}):
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except(TypeError, FileNotFoundError):
+        my_list = []
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+        save_to_json_file(my_list, "add_item.json")
